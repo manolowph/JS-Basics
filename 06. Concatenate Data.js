8d786851a1e0ed6c0f8f3fda98502589b4aa13c9
Ivaylo Manolov <113268249+manolowph@users.noreply.github.com>
2022-09-12 02:33:39 +0300

    Create 06. Concatenate Data.js

---
+++ 06. Concatenate Data.js
@@ -0,0 +1,8 @@
+function concatenateData(input) {
+    let name = input[0];
+    let lastName = input[1];
+    let age = Number(input[2]);
+    let city = input[3];
+
+    console.log(`You are ${name} ${lastName}, a ${age}-years old person from ${city}.`);
+}
