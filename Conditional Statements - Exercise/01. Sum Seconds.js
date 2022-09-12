fc3c1dd282676db68665927254437e46b67a5b6f
Ivaylo Manolov <113268249+manolowph@users.noreply.github.com>
2022-09-12 18:08:02 +0300

    Create 01. Sum Seconds.js

---
+++ Conditional Statements - Exercise/01. Sum Seconds.js
@@ -0,0 +1,18 @@
+function sumSeconds(input) {
+    // Sum Seconds
+
+    let timeOne = Number(input[0]);
+    let timeTwo = Number(input[1]);
+    let timeThree = Number(input[2]);
+
+    let totalTime = timeOne + timeTwo + timeThree;
+    let totalTimeInMin = Math.floor(totalTime / 60);
+    let totalTimeInSec = totalTime % 60;
+
+    if (totalTimeInSec < 10) {
+        console.log(`${totalTimeInMin}:0${totalTimeInSec}`)
+    } else {
+        console.log(`${totalTimeInMin}:${totalTimeInSec}`);
+    }
+
+}
