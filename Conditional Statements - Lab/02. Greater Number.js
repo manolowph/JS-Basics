78dc9f7f76808f0324338fef529b372f3a2d8b59
Ivaylo Manolov <113268249+manolowph@users.noreply.github.com>
2022-09-12 16:26:45 +0300

    Create 02. Greater Number.js

---
+++ Conditional Statements - Lab/02. Greater Number.js
@@ -0,0 +1,11 @@
+function greaterNumber(input) {
+    // Greater Number
+
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    if (num1 > num2) {
+        console.log(num1);
+    } else {
+        console.log(num2);
+    }
+}
